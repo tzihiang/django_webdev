5be7ea32fa2388a4f26c163107144e5100b0153b
Willis Tay <27534078+tzihiang@users.noreply.github.com>
2020-12-30 18:05:12 +0800

    final commit

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 from tinymce.widgets import TinyMCE
 from django.db import models
 
@@ -18,12 +18,17 @@ class TutorialAdmin(admin.ModelAdmin):
         # Tuple of (Name of header, K,V of fields for list of fields)
         ("Title and date", {"fields": [
          "tutorial_title", "tutorial_published"]}),
-        ("Content", {"fields": ["tutorial_content"]})
+        ("URL", {"fields": ["tutorial_slug"]}),
+        ("Content", {"fields": ["tutorial_content"]}),
+        ("Series", {"fields": ["tutorial_series"]})
     ]
 
     formfield_overrides = {
         models.TextField: {'widget': TinyMCE()}
     }
 
+
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
 
 admin.site.register(Tutorial, TutorialAdmin)
